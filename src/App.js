75380b4b8c1a95085babad518735f57f4415b46b
Swadhin425 <62477765+Swadhin425@users.noreply.github.com>
2021-12-05 14:58:14 +0530

    Update App.js
    changing text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello Learn React swadhin nayak
+          hello Wellcome to my Site
         </a>
       </header>
     </div>
